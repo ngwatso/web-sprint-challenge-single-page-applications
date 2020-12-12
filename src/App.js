7339bb2280f7a6eb7d5704492a8c71d58aa2f855
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-11 21:20:34 -0500

    set up component files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+	return (
+		<>
+			<h1>Lambda Eats</h1>
+			<p>Have some pie!</p>
+		</>
+	);
 };
 export default App;
