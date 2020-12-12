3946c343a09b005403e37d38bb266314ca96f0eb
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-11 21:52:13 -0500

    started sketching out form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Route } from "react";
+import Form from "./components/Form";
 
 const App = () => {
 	return (
 		<>
 			<header>
 				<h1>Lambda Eats</h1>
-				<Route></Route>
-				<Route></Route>
+				<button>Home</button>
+				<button>Help</button>
 			</header>
+
+			<div>
+				<Route path="/" component={Form}>
+					Pizza?
+				</Route>
+			</div>
 			<p>Have some pie!</p>
 		</>
 	);
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,1 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+export default function Form() {
+	// TODO Set up state
+	const [formState, setFormState] = useState({
+		size: "",
+		sauce: "",
+		toppings: "",
+		glutenFreeCrust: "",
+		specialInstructions: "",
+		quantity: "",
+	});
+
+	// TODO Set up serverError state
+	// TODO Button endabled state
+	// TODO Set up error state
+	// TODO validation
+	// TODO onChange function
+	// TODO onSubmit function
+	// TODO Schema
+	// TODO useEffect
+	// TODO JSX
+}
