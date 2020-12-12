1af91eae9eb874ce599d28e96b225963f173948f
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-11 21:59:39 -0500

    set up state, validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,15 +6,48 @@ export default function Form() {
 		size: "",
 		sauce: "",
 		toppings: "",
-		glutenFreeCrust: "",
+		glutenFreeCrust: false,
 		specialInstructions: "",
 		quantity: "",
 	});
 
 	// TODO Set up serverError state
-	// TODO Button endabled state
+
+	const [serverError, setServerError] = useState("");
+
+	// TODO Button enabled/disabled state
+
+	const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
+
 	// TODO Set up error state
+
+	const [errors, setErrors] = useState({
+		size: "",
+		sauce: "",
+		toppings: "",
+		glutenFreeCrust: "",
+		specialInstructions: "",
+		quantity: "",
+	});
+
 	// TODO validation
+
+	const validateChange = (e) => {
+		yup.reach(formSchema, e.target.name)
+			.validate(
+				e.target.type === "checkbox"
+					? e.target.checked
+					: e.target.value
+			)
+			.then((valid) => {
+				setErrors({ ...errors, [e.target.name]: "" });
+			})
+			.catch((err) => {
+				console.log("ERROR", err);
+				setErrors({ ...errors, [e.target.name]: err.errors[0] });
+			});
+	};
+
 	// TODO onChange function
 	// TODO onSubmit function
 	// TODO Schema
